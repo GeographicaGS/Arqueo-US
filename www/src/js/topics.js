1c3e8f715eeb3f9a23652d7e6279fe69aaa38ca6
Raúl Yeguas <neokore@gmail.com>
2015-01-29 19:12:53 +0100

    Topics fix

--- www/src/js/topics.js
+++ www/src/js/topics.js
@@ -5,9 +5,9 @@ app.topics = [
 			{
 				id:1,
 				title_es: "Puntos yacimientos",
-				wmsServer:"http://dev.arqueo.geographica.gs/geoserver/arqueo/wms?",
+				wmsServer:"http://arqueo.geographica.gs/geoserver/arqueo/wms?",
 	            wmsLayName:"yacimiento_point_view",
-	            geoNetWk:"http://dev.arqueo.geographica.gs/geonetwork/srv/spa/search?hl=spa#|2486d745-996a-42df-a849-60c0f746dd64",
+	            geoNetWk:"http://arqueo.geographica.gs/geonetwork/srv/spa/search?hl=spa#|2486d745-996a-42df-a849-60c0f746dd64",
 	            files:["fichero1"],
 	            desc_es:"Puntos donde se encuentran los yacimientos.",
 	            dataSource:"None",
@@ -15,9 +15,9 @@ app.topics = [
 	        {
 	        	id:2,
 				title_es: "Polígonos yacimientos",
-				wmsServer:"http://dev.arqueo.geographica.gs/geoserver/arqueo/wms?",
+				wmsServer:"http://arqueo.geographica.gs/geoserver/arqueo/wms?",
 	            wmsLayName:"yacimiento_polygon_view",
-	            geoNetWk:"http://dev.arqueo.geographica.gs/geonetwork/srv/spa/search?hl=spa#|2486d745-996a-42df-a849-60c0f746dd64",
+	            geoNetWk:"http://arqueo.geographica.gs/geonetwork/srv/spa/search?hl=spa#|2486d745-996a-42df-a849-60c0f746dd64",
 	            files:["fichero1"],
 	            desc_es:"Polígonos donde se encuentran los yacimientos.",
 	            dataSource:"None",
